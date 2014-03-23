970d46222026d0a8f0bb786c5f5dc7f910b435cc
Tiffani Ashley Bell <tiffani2k3@gmail.com>
2014-03-23 13:24:52 -0700

    Route updates for projects and questions.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Supply::Application.routes.draw do
-  resources :questions
+  devise_for :users
+  resources :companies
 
-  resources :projects
+  resources :projects do
+    resources :questions
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
