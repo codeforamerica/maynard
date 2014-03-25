6c9c12fff428ae3783b5630b76b5b6ce33dfaa4b
Tiffani Ashley Bell <tiffani2k3@gmail.com>
2014-03-25 13:08:24 -0700

    To assuage Heroku static asset requirements.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,6 @@ Supply::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.serve_static_assets = true
 end
