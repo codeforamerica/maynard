00e052ac4e5d4849a8b720e03197ea9b76951c11
Tiffani Ashley Bell <tiffani2k3@gmail.com>
2014-03-25 13:20:44 -0700

    More Heroku problems.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,9 @@ Supply::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  #config.assets.compile = false
+  config.serve_static_assets = true
+  config.assets.precompile += %w( *.css *.js )
 
   # Generate digests for assets URLs.
   config.assets.digest = true
