b4cb27dd9baf94333e03c1a286a63041de4f1269
Tiffani Ashley Bell <tiffani2k3@gmail.com>
2014-03-26 13:13:47 -0700

    Make sure mail hashes can't be changed on projects.

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -15,4 +15,11 @@ class ProjectTest < ActiveSupport::TestCase
     project = Project.create(name: "Awesome project", project_number: "9999-XL")
     assert !project.mail_hash.blank?
   end
+
+  test "project mail hash shouldn't change" do
+    project = Project.create(name: "Super awesome project", project_number: "9997-PL")
+    assert_raise ActiveRecord::ActiveRecordError do
+      project.update_attribute(:mail_hash, "muppets-89999")
+    end
+  end
 end
