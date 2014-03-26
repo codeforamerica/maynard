8770c88295d82b9531d10d5d781982f5ed5a2c27
Tiffani Ashley Bell <tiffani2k3@gmail.com>
2014-03-26 14:44:49 -0700

    Forgot last changes.

--- app/models/contracting_officer.rb
+++ app/models/contracting_officer.rb
@@ -1,9 +1,9 @@
 class ContractingOfficer < ActiveRecord::Base
   has_many :projects
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  
+  validates :first_name, presence: { message: "must be provided." }
+  validates :last_name, presence: { message: "must be provided." }
+
   def name
     "#{ first_name } #{ last_name }"
   end
