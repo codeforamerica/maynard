f407a58484e47381505d95635bce30edb4a68dfa
Tiffani Ashley Bell <tiffani2k3@gmail.com>
2014-03-26 14:41:03 -0700

    Another test for questions.

--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -4,5 +4,6 @@ class QuestionTest < ActiveSupport::TestCase
   test "Question should have a body" do
     question = Question.new
     assert !question.save, "Saved without an actual question!"
+    assert_equal "can't be blank", question.errors[:body][0]
   end
 end
