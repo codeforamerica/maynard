298b86cb46302390e69e5bf7cb055a67c53e47d3
Tiffani Ashley Bell <tiffani2k3@gmail.com>
2014-03-26 13:08:50 -0700

    Tests and fixes around projects.

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,6 +3,9 @@ class Project < ActiveRecord::Base
 
   belongs_to :contracting_officer, counter_cache: true
 
+  validates :project_number, presence: true
+  validates :name, presence: true
+  
   validates_uniqueness_of :project_number
 
   accepts_nested_attributes_for :contracting_officer
--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class ProjectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "project should have a number" do
+    project = Project.new
+    assert !project.save, "Saved project without a project number"
+  end
+
+  test "project should have a name" do
+    project = Project.new
+    assert !project.save, "Saved project without a name"
+  end
+
+  test "project should have a mail hash when created" do
+    project = Project.create(name: "Awesome project", project_number: "9999-XL")
+    assert !project.mail_hash.blank?
+  end
 end
