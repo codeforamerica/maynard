45d457e5f97f7c6354d5e0dabd3d8b0aee36bed8
Tiffani Ashley Bell <tiffani2k3@gmail.com>
2014-03-26 14:43:11 -0700

    More tests for ContractingOfficer.

--- test/models/contracting_officer_test.rb
+++ test/models/contracting_officer_test.rb
@@ -4,11 +4,13 @@ class ContractingOfficerTest < ActiveSupport::TestCase
   test "make sure contracting officer has a first name" do
     officer = ContractingOfficer.new
     assert !officer.save, "Saved without first name"
+    assert_equal "must be provided.", officer.errors[:first_name][0]
   end
 
   test "make sure contracting officer has a last name" do
     officer = ContractingOfficer.new
     assert !officer.save, "Saved without last name"
+    assert_equal "must be provided.", officer.errors[:last_name][0]
   end
 
   test "make sure full name comes from ContractingOfficer#name" do
