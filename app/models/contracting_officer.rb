f56061cca7e91e190a587a0c83de4ec02b77cd58
Tiffani Ashley Bell <tiffani2k3@gmail.com>
2014-03-26 13:20:23 -0700

    Tests on ContractingOfficer.

--- app/models/contracting_officer.rb
+++ app/models/contracting_officer.rb
@@ -1,5 +1,8 @@
 class ContractingOfficer < ActiveRecord::Base
   has_many :projects
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   
   def name
     "#{ first_name } #{ last_name }"
--- test/models/contracting_officer_test.rb
+++ test/models/contracting_officer_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class ContractingOfficerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "make sure contracting officer has a first name" do
+    officer = ContractingOfficer.new
+    assert !officer.save, "Saved without first name"
+  end
+
+  test "make sure contracting officer has a last name" do
+    officer = ContractingOfficer.new
+    assert !officer.save, "Saved without last name"
+  end
+
+  test "make sure full name comes from ContractingOfficer#name" do
+    officer = ContractingOfficer.create(first_name: "Aubrey", last_name: "Graham")
+    assert_equal "Aubrey Graham", officer.name
+  end
 end
