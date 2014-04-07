2f6750f708aeca84e68b56dd3be50ae2b95ddbfb
Tiffani Ashley Bell <tiffani2k3@gmail.com>
2014-04-07 11:29:43 -0700

    Left off last commit\!

---
+++ app/controllers/plan_holders_controller.rb
@@ -0,0 +1,40 @@
+class PlanHoldersController < ApplicationController
+  def create
+    @planholder = PlanHolder.new(planholders_params)
+
+    respond_to do |wants|
+      if @planholder.save
+        wants.html { redirect_to plan_holders_url, status: :created, notice: "Yay!" }
+      else
+        wants.html { render action: 'new' }
+      end
+    end
+  end
+
+  def new
+    @planholder = PlanHolder.new
+    @planholder.user = User.new
+    @planholder.company = Company.new
+    @projects = Project.order('project_number ASC').pluck(:id, :name, :project_number)
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def destroy
+  end
+
+  private
+  def planholders_params
+    params.require(:plan_holder).permit(:project_id, company_attributes: [:name], user_attributes: [:email])
+  end
+end
