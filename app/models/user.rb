5de60c031c048ff93ffff450c3ac16cd6705a0bb
Tiffani Ashley Bell <tiffani2k3@gmail.com>
2014-04-07 12:48:18 -0700

    Adding form validations\!

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   #devise :ldap_authenticatable, :rememberable, :trackable,
   #       :validatable
+
+  validates :email, presence: { message: "must be provided." }
+  validates :first_name, presence: { message: "must be provided." }
+  validates :last_name, presence: { message: "must be provided." }
 end
