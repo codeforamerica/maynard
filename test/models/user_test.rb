ada5cd61f6e4c8566b7fd30291f169b46fd3516a
Tiffani Ashley Bell <tiffani2k3@gmail.com>
2014-04-07 13:04:30 -0700

    (whoops, versus last commit\!) User tests.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,4 +5,14 @@ class UserTest < ActiveSupport::TestCase
     user = User.new(last_name: "Frog", email: "kermit.the.frog@codeforamerica.org")
     assert !user.save, "Saved without a first name"
   end
+
+  test "should make sure user has a last name" do
+    user = User.new(first_name: "Kermit", email: "kermit.the.frog@codeforamerica.org")
+    assert !user.save, "Saved without a last name"
+  end
+
+  test "should make sure user has an email address" do
+    user = User.new(first_name: "Kermit", last_name: "Frog")
+    assert !user.save, "Saved without an email address"
+  end
 end
